94ab5983f29f66a6a4362f04883ffde0c7d26c87
Daniel Brazel <dbrazel4@gmail.com>
2018-02-04 02:34:11 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -5,3 +5,24 @@ $(document).ready(function(){
 // call functions here
 
 });
+
+$("form").on("submit", function() {
+  if ($("input:forst").val() === "correct") {
+    alert('your form is going to be submitted now');
+    return;
+  }
+  alert("you entered the wrong value");
+  return;
+});
+
+function getIt() {
+  $("p").on("click", function(){
+    alert('Hey!')
+  })
+}
+
+function frameIt() {
+  $("img").on("load", function(){
+    
+  })
+}
