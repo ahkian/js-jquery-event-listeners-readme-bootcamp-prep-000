32f20076d6bd5ddc50ba66c17ff90ea0f412e228
Daniel Brazel <dbrazel4@gmail.com>
2018-02-04 03:29:45 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -22,7 +22,7 @@ function frameIt() {
 }
 
 function pressIt() {
-  $("form #typing").on("keydown", function(key){
+  $("#typing").on("keydown", function(key){
     if (key.which === 71 ){console.log("You pressed g!")}
   })
 }
